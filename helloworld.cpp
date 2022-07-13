db1ce406cfce58041dc2a511a4962b55611d1e52
Peeja Khorashadi <khorashadi.peeja@gmail.com>
2022-07-13 10:19:22 -0600

    first commit

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
-    vector<string> msg {"hello", "c++", "World", "from", "VS code", "and the c++ extension"};
+    // vector<string> msg {"hello", "c++", "World", "from", "VS code", "and the c++ extension"};
 
-    for (const string & word: msg) {
-        cout << word << " ";
-    }
-    cout << endl;
+    // for (const string & word: msg) {
+    //     cout << word << " ";
+    // }
+    // cout << endl;
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r",stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+
+    int n;
+    cin >> n;
+    cout << n;
+
+    return 0;
 }
